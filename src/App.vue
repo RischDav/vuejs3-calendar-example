db8ff03c625a00181a1a7a7943769a75b47d1b5b
David Rischow <drischow@stud.hs-heilbronn.de>
2024-06-02 13:05:58 +0200

    split objects into components

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container-fluid mt-5">
+    <div class="row">
+      <div class="col-12">
+        <!-- Anfang: Template für die Calendar-Week-Component -->
+        <CalendarWeek />
+        <!-- Ende: Template für die Calendar-Week-Component -->
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-4 offset-4">
+        <!-- Anfang: Template für die Calendar-Entry-Component -->
+        <CalendarEntry />
+        <!-- Ende: Template für die Calendar-Entry-Component -->
+      </div>
+      <div class="col-2 offset-2">
+        <div class="float-end">
+          <!-- Mit dem Button blenden wir die Calendar-Settings-Component ein bzw. aus. -->
+          <button class="btn btn-lg mb-2">
+            <i class="fas fa-cogs"></i>
+          </button>
+        </div>
+        <!-- Anfang: Template für die Calendar-Settings-Component -->
+        <CalendarSettings />
+        <!-- Ende: Template für die Calendar-Day-Component -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CalendarEntry from './components/CalendarEntry';
+import CalendarSettings from './components/CalendarSettings';
+import CalendarWeek from './components/CalendarWeek';
+export default {
+  name: "App",
+  components: {
+    CalendarWeek,
+    CalendarEntry,
+    CalendarSettings
+  },
+};
+</script>
+
+<style>
+@import "~bootstrap/dist/css/bootstrap.min.css";
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
+
+.square {
+  width: 40px;
+  height: 40px;
+}
+</style>
---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <div class="card-header text-center" role="button">
+      <strong>Montag</strong>
+    </div>
+    <div class="card-body"></div>
+
+    <div id="calendar-day">
+      <!-- Anfang: Template für die Calendar-Event-Component -->
+      <CalendarEvent />
+      <!-- Ende: Template für die Calendar-Event-Component -->
+    </div>
+  </div>
+</template>
+
+<script>
+import CalendarEvent from "./CalendarEvent";
+export default {
+  name: "CalendarDay",
+  components: {
+    CalendarEvent,
+  },
+};
+</script>
+
+<style scoped></style>
